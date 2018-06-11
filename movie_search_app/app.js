0d656bb47aed1ede28799a17156db4701c33af71
Chris Allen <chrishughallen@gmail.com>
2018-06-11 18:52:10 +0000

    omdb app

---
+++ movie_search_app/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+    res.render("search");
+});
+
+
+
+app.get("/results", function(req,res){
+    var query = req.query.search;
+    var key = "&apikey=thewdb";
+    var url = "http://omdbapi.com/?s=" + query + key
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("results", {data : data});
+        }
+    });
+});
+
+
+app.get("/search", function(req,res){
+    
+})
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("movie app has started!")
+})
